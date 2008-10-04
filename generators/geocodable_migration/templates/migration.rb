52d15a2cee8e1e824ada29a3df703a580ea4d466
Daniel Morrison <daniel@collectiveidea.com>
2008-10-04 16:10:10 -0400

    Added precison to the Geocode model, added new default database indexes and updated the README.

--- generators/geocodable_migration/templates/migration.rb
+++ generators/geocodable_migration/templates/migration.rb
@@ -9,11 +9,18 @@ class <%= class_name %> < ActiveRecord::Migration
       t.column "region", :string
       t.column "postal_code", :string
       t.column "country", :string
+      t.column "precision", :string
     end
 
     add_index "geocodes", ["longitude"], :name => "geocodes_longitude_index"
     add_index "geocodes", ["latitude"], :name => "geocodes_latitude_index"
     add_index "geocodes", ["query"], :name => "geocodes_query_index", :unique => true
+    add_index "geocodes", ["locality"], :name => "geocodes_locality_index"
+    add_index "geocodes", ["region"], :name => "geocodes_region_index"
+    add_index "geocodes", ["postal_code"], :name => "geocodes_postal_code_index"
+    add_index "geocodes", ["country"], :name => "geocodes_country_index"
+    add_index "geocodes", ["precision"], :name => "geocodes_precision_index"
+    
 
     create_table "geocodings" do |t|
       t.column "geocodable_id", :integer
