dede660bb5c4c4f5a329f3f1ac8d9a1aaa67f749
Brandon Keepers <brandon@collectiveidea.com>
2009-05-23 23:48:01 -0400

    Only rescue Graticule errors and log them

--- lib/geocode.rb
+++ lib/geocode.rb
@@ -29,7 +29,8 @@ class Geocode < ActiveRecord::Base
   
   def self.create_from_location(location)
     create geocoder.locate(location).attributes.merge(:query => location.to_s)
-  rescue
+  rescue Graticule::Error => e
+    logger.warn e.message
     nil
   end
   
