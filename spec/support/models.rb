4b3e1b6143ea0678ad9a812566bb61397037b4e1
Brian Ryckbost <bryckbost@gmail.com>
2010-09-24 10:42:35 -0400

    Cleaned up spec helper

---
+++ spec/support/models.rb
@@ -0,0 +1,35 @@
+class Vacation < ActiveRecord::Base
+  acts_as_geocodable :normalize_address => true
+  belongs_to :nearest_city, :class_name => 'City', :foreign_key => 'city_id'
+end
+
+class Staycation < ActiveRecord::Base
+  set_table_name 'vacations'
+
+  acts_as_geocodable
+  validates_as_geocodable(:allow_nil => false) do |geocode|
+    ["USA", "US"].include?(geocode.country)
+  end
+end
+
+class City < ActiveRecord::Base
+  acts_as_geocodable :address => {:postal_code => :zip}
+end
+
+class ValidatedVacation < ActiveRecord::Base
+  acts_as_geocodable
+  validates_as_geocodable
+end
+
+class AddressBlobVacation < ActiveRecord::Base
+  acts_as_geocodable :address => :address, :normalize_address => true
+end
+
+class CallbackLocation < ActiveRecord::Base
+  acts_as_geocodable :address => :address
+  set_callback :geocoding, :after, :done_geocoding
+
+  def done_geocoding
+    true
+  end
+end
