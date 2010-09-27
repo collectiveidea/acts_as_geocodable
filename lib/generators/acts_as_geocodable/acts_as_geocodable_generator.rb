0c8ebead765f2559f211a43c69d82d5400b51496
Brian Ryckbost <bryckbost@gmail.com>
2010-09-27 08:30:55 -0400

    Add Rails 3 generator

---
+++ lib/generators/acts_as_geocodable/acts_as_geocodable_generator.rb
@@ -0,0 +1,28 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+
+class ActsAsGeocodableGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+
+  def self.source_root
+     @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+  end
+
+  # Implement the required interface for Rails::Generators::Migration.
+  #
+  def self.next_migration_number(dirname) #:nodoc:
+    next_migration_number = current_migration_number(dirname) + 1
+    if ActiveRecord::Base.timestamped_migrations
+      [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
+    else
+      "%.3d" % next_migration_number
+    end
+  end
+
+  def create_migration_file
+    if defined?(ActiveRecord)
+      migration_template 'migration.rb', 'db/migrate/add_geocodable_tables.rb'
+    end
+  end
+
+end
