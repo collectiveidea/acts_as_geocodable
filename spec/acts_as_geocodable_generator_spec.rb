ef838fef1d3868707ba9aa351b3ebb22be28aeca
Brian Ryckbost <bryckbost@gmail.com>
2010-09-28 12:19:12 -0400

    Delete commented line

--- spec/acts_as_geocodable_generator_spec.rb
+++ spec/acts_as_geocodable_generator_spec.rb
@@ -16,7 +16,6 @@ describe 'ActsAsGeocodableGenerator' do
   it "should generate the correct file" do
     pending    
     Rails::Generators.invoke "acts_as_geocodable", :destination => fake_rails_root
-    # Rails::Generators::Scripts::Generate.new.run(["acts_as_geocodable"], :destination => fake_rails_root)
     new_file = (file_list - @original_files).first
     File.basename(new_file).should =~ /add_geocodable_tables/
   end
