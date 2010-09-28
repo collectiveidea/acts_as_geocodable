8b27c51f2076830fcab2b78f550be4d5c214d217
Brian Ryckbost <bryckbost@gmail.com>
2010-09-28 11:42:32 -0400

    Adding a pending spec to test the generation of a migration

---
+++ spec/acts_as_geocodable_generator_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'rails/generators'
+# require 'rails/generators/scripts/generate'
+
+describe 'ActsAsGeocodableGenerator' do
+
+  before do
+    FileUtils.mkdir_p(fake_rails_root)
+    @original_files = file_list
+  end
+
+  after do
+    FileUtils.rm_r(fake_rails_root)
+  end
+
+  it "should generate the correct file" do
+    pending    
+    Rails::Generators.invoke "acts_as_geocodable", :destination => fake_rails_root
+    # Rails::Generators::Scripts::Generate.new.run(["acts_as_geocodable"], :destination => fake_rails_root)
+    new_file = (file_list - @original_files).first
+    File.basename(new_file).should =~ /add_geocodable_tables/
+  end
+
+  private
+    def fake_rails_root
+      File.join(File.dirname(__FILE__), 'rails_root')
+    end
+
+    def file_list
+      Dir.glob(File.join(fake_rails_root, "*"))
+    end
+end
