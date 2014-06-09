74418daed3b6472f1e225a189d3786cfc7604451
Steve Richert <steve.richert@gmail.com>
2014-06-09 12:03:16 -0400

    Default to SQLite when running the test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ Bundler.require(:test)
 plugin_test_dir = File.dirname(__FILE__)
 ActiveRecord::Base.logger = Logger.new(plugin_test_dir + "/debug.log")
 ActiveRecord::Base.configurations = YAML::load(IO.read(plugin_test_dir + "/db/database.yml"))
-ActiveRecord::Base.establish_connection(ENV["DB"] || "mysql")
+ActiveRecord::Base.establish_connection(ENV["DB"] || "sqlite")
 ActiveRecord::Migration.verbose = false
 load(File.join(plugin_test_dir, "db", "schema.rb"))
 
